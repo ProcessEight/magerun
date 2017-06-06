f9fc0345560507a63161c560e27591e11e6f2ee1
sfrost_purenet <simon.frost@purenet.co.uk>
2017-06-06 16:00:38 +0100

    dev-module-rewrite-list-better: Added original class name and original file path to table

--- src/ProjectEight/Magento/Command/Developer/Module/RewriteList/BetterCommand.php
+++ src/ProjectEight/Magento/Command/Developer/Module/RewriteList/BetterCommand.php
@@ -8,6 +8,8 @@ use N98\Util\Console\Helper\TableHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\StringInput;
+use Symfony\Component\Console\Output\BufferedOutput;
 
 class BetterCommand extends AbstractRewriteCommand
 {
@@ -18,7 +20,7 @@ class BetterCommand extends AbstractRewriteCommand
             ->setDescription('Lists all magento rewrites, with extra info')
             ->addOption(
                 'format',
-                null,
+                NULL,
                 InputOption::VALUE_OPTIONAL,
                 'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
             )
@@ -38,22 +40,75 @@ class BetterCommand extends AbstractRewriteCommand
             return;
         }
 
-        $rewrites = array_merge($this->loadRewrites(), $this->loadAutoloaderRewrites());
-
         $table = array();
-        foreach ($rewrites as $type => $data) {
+        foreach ($this->loadRewrites() as $type => $data) {
             if (count($data) > 0) {
                 foreach ($data as $class => $rewriteClass) {
                     $table[] = array(
-//                        $type,
+                        $type,
                         $class,
+                        '',
                         implode(', ', $rewriteClass),
                     );
                 }
             }
         }
 
-        if (count($table) === 0 && $input->getOption('format') === null) {
+        try {
+
+            /*
+             * Translate group class name
+             */
+
+            // ensure that n98-magerun doesn't stop after first command
+            $this->getApplication()->setAutoExit(false);
+
+            /*
+             * Use existing dev:class:lookup command
+             */
+
+            foreach ($table as $row => $rewriteMetadata) {
+                if ($rewriteMetadata[0] == "autoload: Mage") {
+                    unset($table[ $row ]);
+                    continue;
+                }
+
+                if (isset($rewriteMetadata[0]) && isset($rewriteMetadata[1])) {
+                    $commandOptions = "";
+                    $commandOptions .= $rewriteMetadata[0] . " ";                        // type
+                    $commandOptions .= explode("\n", $rewriteMetadata[1])[0];   // name
+
+                    // Run command
+                    $devClassLookupInput = new StringInput("dev:class:lookup {$commandOptions}");
+                    $bufferedOutput      = new BufferedOutput();
+                    $this->getApplication()->run($devClassLookupInput, $bufferedOutput);
+                    // Get output of command
+                    $commandOutput = $bufferedOutput->fetch();
+                    // Generate core class name
+                    $commandOutput = explode("\n", $commandOutput);
+                    $className     = trim(substr($commandOutput[0], strrpos($commandOutput[0], ' ')));
+                    $singular      = ucwords(trim($rewriteMetadata[0], 's'));
+                    $className     = str_replace(ucwords($rewriteMetadata[0]), $singular, $className);
+
+                    if(substr($className, 0, 15) == "Mage_Enterprise") {
+                        $className = str_replace("Mage_", "", $className);
+                    }
+
+                    $filepath = "app/code/core/" . str_replace('_', "/", $className) . ".php";
+
+                    $table[ $row ][2] = $className;
+                    $table[ $row ][] = $filepath;
+                }
+            }
+
+            // reactivate auto-exit
+            $this->getApplication()->setAutoExit(true);
+
+        } catch (Exception $e) {
+            $output->writeln('<error>' . $e->getMessage() . '</error>');
+        }
+
+        if (count($table) === 0 && $input->getOption('format') === NULL) {
             $output->writeln('<info>No rewrites were found.</info>');
         } else {
             if (count($table) == 0) {
@@ -62,9 +117,10 @@ class BetterCommand extends AbstractRewriteCommand
             /* @var $tableHelper TableHelper */
             $tableHelper = $this->getHelper('table');
             $tableHelper
-                ->setHeaders(array('Class', 'Rewrite'))
+                ->setHeaders(array('Type', 'Grouped Class Name', 'Original Class', 'Rewritten Class', 'File path'))
                 ->setRows($table)
-                ->renderByFormat($output, $table, $input->getOption('format'));
+                ->renderByFormat($output, $table, $input->getOption('format'))
+            ;
         }
     }
 }
