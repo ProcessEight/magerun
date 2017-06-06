bc0fe27ad58103006bfad83e56be06e9a6600d63
sfrost_purenet <simon.frost@purenet.co.uk>
2017-06-06 14:44:43 +0100

    dev-module-rewrite-list-better: Added command skeleton; Updated .n98-magerun.yaml file

---
+++ src/ProjectEight/Magento/Command/Developer/Module/RewriteList/BetterCommand.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace ProjectEight\Magento\Command\Developer\Module\RewriteList;
+
+use N98\Magento\Command\Developer\Module\Rewrite\AbstractRewriteCommand;
+use N98\Util\Console\Helper\Table\Renderer\RendererFactory;
+use N98\Util\Console\Helper\TableHelper;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class BetterCommand extends AbstractRewriteCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('dev:module:rewrite:list:better')
+            ->setDescription('Lists all magento rewrites, with extra info')
+            ->addOption(
+                'format',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'Output Format. One of [' . implode(',', RendererFactory::getFormats()) . ']'
+            )
+        ;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     *
+     * @return int|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->detectMagento($output, true);
+        if (!$this->initMagento()) {
+            return;
+        }
+
+        $rewrites = array_merge($this->loadRewrites(), $this->loadAutoloaderRewrites());
+
+        $table = array();
+        foreach ($rewrites as $type => $data) {
+            if (count($data) > 0) {
+                foreach ($data as $class => $rewriteClass) {
+                    $table[] = array(
+//                        $type,
+                        $class,
+                        implode(', ', $rewriteClass),
+                    );
+                }
+            }
+        }
+
+        if (count($table) === 0 && $input->getOption('format') === null) {
+            $output->writeln('<info>No rewrites were found.</info>');
+        } else {
+            if (count($table) == 0) {
+                $table = array();
+            }
+            /* @var $tableHelper TableHelper */
+            $tableHelper = $this->getHelper('table');
+            $tableHelper
+                ->setHeaders(array('Class', 'Rewrite'))
+                ->setRows($table)
+                ->renderByFormat($output, $table, $input->getOption('format'));
+        }
+    }
+}
