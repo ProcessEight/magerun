4cf439999dd86596bcb2f19d73fb77d2c346affa
ProjectEight <s.frost@frostnet.co.uk>
2017-10-04 20:36:24 +0100

    sys:report: Refactored EditionReport so it no longer relies on Mage::getEdition() method

--- src/ProjectEight/Magento/Command/System/Report/System/EditionReport.php
+++ src/ProjectEight/Magento/Command/System/Report/System/EditionReport.php
@@ -5,14 +5,22 @@ namespace ProjectEight\Magento\Command\System\Report\System;
 use ProjectEight\Magento\Command\System\Report\Result;
 use ProjectEight\Magento\Command\System\Report\ResultCollection;
 use ProjectEight\Magento\Command\System\Report\SimpleReport;
+use N98\Magento\Command\CommandAware;
+use Symfony\Component\Console\Command\Command;
+use ProjectEight\Magento\Command\System\ReportCommand;
 
 /**
  * Class EditionReport
  *
  * @package ProjectEight\Magento\Command\System\Report\System
  */
-class EditionReport implements SimpleReport
+class EditionReport implements SimpleReport, CommandAware
 {
+    /**
+     * @var ReportCommand
+     */
+    protected $reportCommand;
+
     /**
      * @param ResultCollection $results
      *
@@ -22,10 +30,21 @@ class EditionReport implements SimpleReport
     {
         $result = $results->createResult();
 
-        $edition = \Mage::getEdition();
+        $edition = 'Community';
+        if($this->reportCommand->getApplication()->isMagentoEnterprise()) {
+            $edition = 'Enterprise';
+        }
 
         $result->setStatus(Result::STATUS_INFO);
         $result->setMessage("<info>Edition: <comment>$edition</comment>. </info>");
+    }
+
+    /**
+     * @param Command $command
+     */
+    public function setCommand(Command $command)
+    {
+        $this->reportCommand = $command;
     }
 
 }
